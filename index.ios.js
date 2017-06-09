3bb0375296fb3dc0962022d419c9da8235cc5fab
Aaron <aaronvine@gmail.com>
2017-06-09 10:54:59 +0300

    add recipe list

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import RecipeList from './src/components/RecipeList';
 
-const App = () => <Header headerText={'Recipes'} />;
+const App = () => (
+  <View>
+    <Header headerText={'Recipes'} />
+    <RecipeList />
+  </View>
+);
 
 AppRegistry.registerComponent('albums', () => App);
